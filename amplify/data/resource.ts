e0afaa2c8528a08999c35d5e41e996543b20791f
John Guy <john@jguy.net>
2024-09-02 15:52:24 -0700

    update schema

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,11 +7,30 @@ specifies that any unauthenticated user can "create", "read", "update",
 and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
+  Person: a
     .model({
-      content: a.string(),
+      name: a.string().required(),
+      description: a.string(),
     })
-    .authorization((allow) => [allow.guest()]),
+    .authorization((allow) => [allow.owner()]),
+  Place: a
+    .model({
+      name: a.string(),
+      description: a.string(),
+    })
+    .authorization((allow) => [allow.owner()]),
+  Thing: a
+    .model({
+      name: a.string(),
+      description: a.string(),
+    })
+    .authorization((allow) => [allow.owner()]),
+  Idea: a
+    .model({
+      name: a.string(),
+      description: a.string(),
+    })
+    .authorization((allow) => [allow.owner()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -19,7 +38,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'iam',
+    defaultAuthorizationMode: 'userPool',
   },
 });
 
